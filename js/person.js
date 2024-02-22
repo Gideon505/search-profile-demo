62edce1836347e85d80ff17a6200ac51ca52a71e
Gideon Shobowale <gideonshobowale@yahoo.co.uk>
2024-02-22 00:28:17 +0000

    get input and dsiplay in empty string

---
+++ js/person.js
@@ -0,0 +1,20 @@
+//  Default person object
+const person = [
+
+    {
+        name : 'Dexter',
+        age : 20
+    },
+    {
+        name : 'Philip',
+        age : 30
+    },
+    {
+        name : 'Jordan',
+        age : 50
+    }
+
+];
+
+
+export default person;

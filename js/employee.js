32c4618e8e95fb1d200249ee48a360c3ff32e2ad
Gideon Shobowale <gideonshobowale@yahoo.co.uk>
2024-06-13 02:34:07 +0100

    new js file added

---
+++ js/employee.js
@@ -0,0 +1,18 @@
+const employee = [{
+    "first_name": "Dexter",
+    "last_name": "Doe",
+    "age:": 20
+},
+{
+    "first_name": "Philip",
+    "last_name": "Doe",
+    "age:": 30
+},
+{
+    "first_name": "Jordan",
+    "last_name": "Doe",
+    "age:": 50
+}
+]
+
+export default employee;
